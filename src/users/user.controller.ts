973c202abcf1028d7d7a793e536beeeeccd4d199
Fredi Anriko <fredianriko@gmail.com>
2022-01-20 13:01:34 +0700

    final commit 1

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,12 +1,60 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  UseFilters,
+  Body,
+  Put,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { CreateUserDto } from './create-user.dto';
+import { EntityNotFoundExceptionFilter } from './entity-not-found-exception-filter';
 import { UserService } from './user.service';
 
 @Controller('users')
+@UseFilters(new EntityNotFoundExceptionFilter())
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  // get all user data
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  async findAll() {
+    return {
+      data: await this.userService.findAll(),
+    };
+  }
+
+  // get user data based on id
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return {
+      data: await this.userService.findOne(id),
+    };
+  }
+
+  // create new user
+  @Post('/register')
+  async create(@Body() data: CreateUserDto) {
+    return {
+      data: await this.userService.create(data),
+    };
+  }
+
+  // update user information based on id
+  @Put(':id')
+  async update(@Body() data: CreateUserDto, @Param('id') id: number) {
+    return {
+      data: await this.userService.update(data, id),
+    };
+  }
+
+  // delete user information based on id
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id') id: number) {
+    await this.userService.delete(id);
   }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -11,5 +12,24 @@ export class UserService {
 
   findAll() {
     return this.userRepository.find();
+  }
+  findOne(id: number) {
+    return this.userRepository.findOneOrFail(id);
+  }
+
+  create(data: CreateUserDto) {
+    const user = new User();
+    user.firstName = data.firstName;
+    user.lastName = data.lastName;
+    user.isActive = data.isActive;
+    return this.userRepository.save(user);
+  }
+
+  update(data: CreateUserDto, id: number) {
+    return this.userRepository.save({ ...data, id: Number(id) });
+  }
+
+  delete(id: number) {
+    return this.userRepository.delete(id);
   }
 }
